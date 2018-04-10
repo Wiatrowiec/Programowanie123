7f2758a18d0b8035af64ee7f51c32b738f7220f7
Wiatrowiec <krysztoszek@wp.pl>
2018-04-10 12:51:25 +0200

    Initial commit

---
+++ C07/main.c
@@ -0,0 +1,20 @@
+//#include "Z00.txt" // 5.2.1
+//#include "Z01.txt" //5.2.2
+//#include "Z02.txt" //5.2.3
+//#include "Z03.txt" //5.2.5
+//#include "Z04.txt" //5.2.6
+//#include "Z05.txt" //5.2.7
+//#include "Z06.txt" //5.2.8
+//#include "Z07.txt" //5.2.9
+//#include "Z08.txt" //5.2.4
+//#include "Z09.txt"
+//#include "Z10.txt"
+//#include "Z11.txt"
+//#include "Z12.txt"
+//#include "Z13.txt"
+//#include "Z14.txt"
+//#include "Z15.txt"
+//#include "Z16.txt"
+//#include "Z17.txt"
+//#include "Z19.txt"
+//#include "Z20.txt"
